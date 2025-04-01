c90040a02ba90592c35b04f575250a1f584a384d
Dariusz Grzegorczyk <d.grzeogrczyk@outlook.com>
2025-04-01 11:47:52 +0200

    some fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from 'next/script'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
@@ -42,8 +43,24 @@ export default function RootLayout({
           type="text/javascript"
           async
         ></script>
+       
+         {/* Google Tag Manager */}
+         <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W72NWNSH');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
       </head>
       <body className="font-sans antialiased">
+
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W72NWNSH"
+      height="0" width="0"  style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         <Header />
         {children}
         <TopFooter />
