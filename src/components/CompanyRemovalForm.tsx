c3e6297e991be9015107a203540254a0897428b1
Dariusz Grzegorczyk <d.grzeogrczyk@outlook.com>
2025-04-01 23:20:36 +0200

    test

--- src/components/CompanyRemovalForm.tsx
+++ src/components/CompanyRemovalForm.tsx
@@ -112,7 +112,7 @@ export default function CompanyFormRemoval() {
     ) {
       return 69900;
     }
-    return 69900;
+    return 100;
   };
 
   const totalPrice = removals.reduce((sum, r) => sum + calculatePriceForLink(r.url), 0);
