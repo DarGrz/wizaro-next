9a7d6376f9475ec2e44aec20ae84c2805b34240e
Dariusz Grzegorczyk <d.grzeogrczyk@outlook.com>
2025-06-01 16:17:54 +0200

    policy

--- src/app/polityka-prywatnosci/page.tsx
+++ src/app/polityka-prywatnosci/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import Script from "next/script";
+
 export default function PolitykaPrywatnosciPage() {
   return (
-    <div className="max-w-5xl mx-auto  h-screen py-20 px-6 text-gray-800">
+    <div className="max-w-5xl mx-auto h-screen py-20 px-6 text-gray-800">
       <h1 className="text-3xl font-semibold mb-6 text-[#002a5c]">Polityka prywatności</h1>
 
       <p className="mb-4">
@@ -19,7 +21,16 @@ export default function PolitykaPrywatnosciPage() {
         <li>Użytkownik ma prawo do wglądu w swoje dane, ich poprawienia oraz żądania usunięcia.</li>
       </ul>
 
-      <script id="CookieDeclaration" src="https://consent.cookiebot.com/39c85a43-3ea8-4832-b299-aadec8adf0fc/cd.js" type="text/javascript" async></script>
+      {/* Cookiebot deklaracja */}
+      <div className="mt-8">
+        <Script
+          id="CookieDeclaration"
+          src="https://consent.cookiebot.com/39c85a43-3ea8-4832-b299-aadec8adf0fc/cd.js"
+          type="text/javascript"
+          async
+        />
+        <div id="CookieDeclaration"></div>
+      </div>
     </div>
   );
 }
