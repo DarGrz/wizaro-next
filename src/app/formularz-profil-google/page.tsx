94f2601a80b8f55c8037aa755c8d62a361959f33
Dariusz Grzegorczyk <d.grzeogrczyk@outlook.com>
2025-06-15 13:07:26 +0200

    fixed view

--- src/app/formularz-profil-google/page.tsx
+++ src/app/formularz-profil-google/page.tsx
@@ -7,8 +7,8 @@ export default function GoogleRemovalPage() {
   return (
     <>
       
-      <main className="min-h-screen  bg-gray-50">
-        <div className="container mx-auto px-8">
+      <main>
+        <div>
           {/* <h1 className="text-3xl font-bold text-center mb-8 text-[#002a5c]">
             Usuwanie wizytówki Google
           </h1> */}
