aefa8fb823b0fa8c046338e6abba3bb50e49d7bf
Dariusz Grzegorczyk <d.grzeogrczyk@outlook.com>
2025-07-03 20:45:05 +0200

    color unifcation

--- src/components/Explenations/GoogleReviewFormExplenation.tsx
+++ src/components/Explenations/GoogleReviewFormExplenation.tsx
@@ -1,3 +1,4 @@
+
 export default function ReviewFormExplenation() {
     return (
       <section className="text-gray-800 p-4 rounded-xl max-w-2xl mx-auto text-sm">
@@ -9,31 +10,41 @@ export default function ReviewFormExplenation() {
   {/* Krok 1 */}
   <li className="bg-white p-3">
     <p className="font-medium mb-1">1. Wyszukaj firmę</p>
-    <p className="text-gray-700">Wpisz nazwę firmy – wybierz ją z listy podpowiedzi.</p>
+     <ul className="list-disc list-inside space-y-0.5 text-gray-700">
+      <li>Wpisz nazwę firmy – wybierz ją z listy podpowiedzi.</li>
+      <li>Jeśli nie możesz znależć firmy skorzystaj z <a className="text-[#2D5CF2]" href="/formularz-opinie">formularza ogólnego</a></li>
+    </ul>
+    
   </li>
 
   {/* Krok 2 */}
   <li className="bg-white p-3">
-    <p className="font-medium mb-1">2. Podaj dane opinii</p>
+    <p className="font-medium mb-1">2. Sprawdź informacje</p>
     <ul className="list-disc list-inside space-y-0.5 text-gray-700">
-      <li>Imię lub pseudonim autora – dokładnie jak w opinii</li>
-      <li>Treść opinii – skopiuj bez zmian</li>
+      <li>Upewnij się, że  wybrałeś właściwą firmę</li>
     </ul>
   </li>
 
+
   {/* Krok 3 */}
   <li className="bg-white p-3">
-    <p className="font-medium mb-1">3. Dodaj link i datę</p>
-    <ul className="list-disc list-inside space-y-0.5 text-gray-700">
-      <li>Link do opinii – jeśli dostępny</li>
-      <li>Data dodania – przybliżona</li>
+    <p className="font-medium mb-1">3. Przejdź dalej</p>
+     <ul className="list-disc list-inside space-y-0.5 text-gray-700">
+      <li>Kliknij przycisk &quot;Dodaj opinie do usunięcie&quot;, aktualna cena pokaże się automatycznie.</li>
     </ul>
+    
   </li>
 
   {/* Krok 4 */}
   <li className="bg-white p-3">
-    <p className="font-medium mb-1">4. Dodaj kolejne lub przejdź dalej</p>
-    <p className="text-gray-700">Możesz dodać więcej opinii lub zakończyć zgłoszenie.</p>
+    <p className="font-medium mb-1">3. Wpisz opinie do usunięcia</p>
+     <ul className="list-disc list-inside space-y-0.5 text-gray-700">
+      <li>Podaj autora i treść opinii</li>
+      <li>Opcjonalnie wybierz datę dodania opinii</li>
+      <li>Możesz dodać na raz więcej opinii, klikając przycisk &quot;Dodaj koljeną opinię&quot;.</li>
+      <li>Sprawdź cenę. Jeśli wszysto sie zgadza, przejdź dalej.</li>
+    </ul>
+    
   </li>
 </ol>
 
