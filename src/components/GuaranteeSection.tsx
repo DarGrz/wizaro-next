b47fd9aef1befecc897303ebf4ab305ba9f9f8ea
Dariusz Grzegorczyk <d.grzeogrczyk@outlook.com>
2025-08-23 12:10:58 +0200

    some fixes

--- src/components/GuaranteeSection.tsx
+++ src/components/GuaranteeSection.tsx
@@ -144,12 +144,11 @@ export default function GuaranteeSection() {
         <div className=" backdrop-blur-sm p-6 rounded-lg  w-full">
           <p className="text-gray-800 text-lg mb-3">
             <span className="font-semibold text-gray-900 inline-block border-b-2 border-[#5BA155] pb-1">
-            Opłata tylko jeśli usuniemy opinię.
+            Bez zaliczek i ukrytych kosztów.
             </span>
           </p>
           
-          <p className="text-gray-600">Bez zaliczek i ukrytych kosztów.
-          </p>
+        
           
          
         </div>
